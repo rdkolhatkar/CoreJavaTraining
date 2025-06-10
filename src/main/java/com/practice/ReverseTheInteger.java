f6194847a293af2af5fa94803b8911908a2dbb50
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-10 22:27:44 +0530

    reverse the integer logic

--- src/main/java/com/practice/ReverseTheInteger.java
+++ src/main/java/com/practice/ReverseTheInteger.java
@@ -3,12 +3,17 @@ package com.practice;
 public class ReverseTheInteger {
     public static void main(String[] args){
 
-        int num = 123456789;
-        int reverseNumber = 0;
+        // Solution 1
+        long num = 123456789;
+        long reverseNumber = 0;
         while(num != 0){
             reverseNumber = reverseNumber * 10 + num % 10;
             num = num / 10;
         }
         System.out.println("Reverse Number is :: "+ reverseNumber);
+
+        // Solution 2
+        long number = 123456789;
+        System.out.println(new StringBuffer(String.valueOf(number)).reverse());
     }
 }
